f49ecdc6515a05af942d94ad55cf879e574db13b
Areeb <areebkhan274427@gmail.com>
2023-02-19 21:50:27 +0530

    minor fix 3

--- src/main/java/com/example/BookMyShowProject/BookMyShowProjectApplication.java
+++ src/main/java/com/example/BookMyShowProject/BookMyShowProjectApplication.java
@@ -1,11 +1,18 @@
 package com.example.BookMyShowProject;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition
+//@OpenAPIDefinition
+@OpenAPIDefinition(servers = {@Server(url = "https://{host}",
+		variables = @ServerVariable(name = "host",
+				defaultValue = "hostname_sit",
+				allowableValues = {"hostname_sit","hostname_uat"}),
+		description = "Host name by environment")})
 public class BookMyShowProjectApplication {
 
 	public static void main(String[] args) {
