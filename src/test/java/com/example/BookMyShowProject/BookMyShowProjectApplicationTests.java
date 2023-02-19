429f5ed71af15b9ac5fdbf02b5e60a4be6c8ec5e
Areeb <areebkhan274427@gmail.com>
2023-02-19 17:19:44 +0530

    master Update lombok version

--- src/test/java/com/example/BookMyShowProject/BookMyShowProjectApplicationTests.java
+++ src/test/java/com/example/BookMyShowProject/BookMyShowProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BookMyShowProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
